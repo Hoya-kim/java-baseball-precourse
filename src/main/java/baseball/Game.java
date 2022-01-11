270423957e88c640d188f998a299dfe95d073ed4
Hoya-kim <1203play@gmail.com>
2022-01-11 11:37:03 +0900

    feat: Add function for restart logic

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,5 +1,8 @@
 package baseball;
 
+import java.util.InputMismatchException;
+import nextstep.utils.Console;
+
 import static utils.Constant.*;
 
 public class Game {
@@ -16,10 +19,27 @@ public class Game {
     // Start game process
     public void run() {
         do {
-            // @TODO: Implement game process
-
-            // @TODO: Set conditionally
-            this.gameStatus = END;
+            startGame();
+            // Request command & Get game status
+            this.gameStatus = requestRestart();
         } while(this.gameStatus == ON_GOING);
+    }
+
+    private void startGame() {
+        // Finished, if correct answer
+    }
+
+    private int requestRestart() {
+        System.out.println("게임을 새로 시작하려면 " + RESTART_GAME + ", 종료하려면 " + STOP_GAME + "를 입력하세요.");
+        while(true) {
+            try {
+                String command = Console.readLine();
+                if(command.equals(RESTART_GAME)) return ON_GOING;
+                else if (command.equals(STOP_GAME)) return END;
+                else throw new InputMismatchException();
+            } catch (InputMismatchException e) {
+                System.out.println("[ERROR] 잘못된 입력입니다.");
+            }
+        }
     }
 }
