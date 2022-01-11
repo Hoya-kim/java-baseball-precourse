3e40403b279030b43bb65802b1ff0f82942db478
Hoya-kim <1203play@gmail.com>
2022-01-11 16:41:13 +0900

    chore: Delete unnecessary comments

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,5 @@
 package baseball;
 
-import java.util.InputMismatchException;
-//import nextstep.utils.Console;
-//import nextstep.utils.Randoms;
-//
-//import static utils.Constant.*;
-
-
 public class Application {
     public static void main(String[] args) {
         Game baseballGame = new Game(new Computer(), new Player());
