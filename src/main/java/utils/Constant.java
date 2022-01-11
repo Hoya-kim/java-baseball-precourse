e25356ee9c2b3cbecd99837ceab37bec871effa1
Hoya-kim <1203play@gmail.com>
2022-01-11 10:47:34 +0900

    feat: Add constants for baseball game
    - Integers, Strings for processing game

---
+++ src/main/java/utils/Constant.java
@@ -0,0 +1,20 @@
+package utils;
+
+/**
+ * Constants for baseball game
+ */
+public class Constant {
+    public static final int BASE_LENGTH = 3;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 1;
+
+    public static final String ANSWER = String.valueOf(BASE_LENGTH);
+
+    // Game status
+    public static final int ON_GOING = 1;
+    public static final int END = -1;
+
+    // Game commands
+    public static final String RESTART_GAME = "1";
+    public static final String STOP_GAME = "2";
+}
